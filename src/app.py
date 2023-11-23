ca1a25e4b1e2d469c34b133ba5ffc796e6dee3ba
Daniil-Solo <daniil.solo1723@gmail.com>
2023-11-23 19:24:48 +0300

    add logging

--- src/app.py
+++ src/app.py
@@ -1,13 +1,28 @@
 """Application"""
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
+from loguru import logger
 
 from src.api.router import router
 
+
+@asynccontextmanager
+def start(application: FastAPI):
+    """
+    Executes start actions
+    """
+    logger.info("Start application")
+    yield
+
+
 app = FastAPI(
+    lifespan=start,
     title="Sign Detector Backend",
     version="0.0.1",
     docs_url="/docs",
     redoc_url="/docs/redoc",
 )
+logger.add("logs.log")
+
 
 app.include_router(router)
