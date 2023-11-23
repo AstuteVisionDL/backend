d29d0924e04b16a055ce5a5ca1d31de296912291
Daniil-Solo <daniil.solo1723@gmail.com>
2023-11-23 23:13:45 +0300

    correct fixture for using startup actions

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,14 @@
 import pytest
 from fastapi.testclient import TestClient
 from src.app import app
+from src.models.dependencies import get_test_sign_detector
 
 
 @pytest.fixture
 def client() -> TestClient:
-    return TestClient(app=app)
+    """
+    Gets test client with test sign detector
+    """
+    app.state.sign_detector = get_test_sign_detector()
+    with TestClient(app) as current_client:
+        yield current_client
