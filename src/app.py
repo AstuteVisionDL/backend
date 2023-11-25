f26988c2bf73fd1cc89bbcb5cdc5313c43bcfc5b
Daniil-Solo <daniil.solo1723@gmail.com>
2023-11-26 00:06:21 +0300

    add static dir, templates dir and endpoint for homepage

--- src/app.py
+++ src/app.py
@@ -1,11 +1,14 @@
 """Application"""
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from loguru import logger
 
-from src.api.router import router
+from src.api.router import router as api_router
+from src.frontend.router import router as frontend_router
 from src.models.dependencies import get_actual_sign_detector
 from src.config import settings, ModeEnum
+from src.constants import STATIC_PATH
 
 
 @asynccontextmanager
@@ -13,6 +16,7 @@ async def lifespan(application: FastAPI):
     """
     Executes start actions
     """
+    logger.info(f"Server has started in mode: {settings.mode}")
     application.state.sign_detector = get_actual_sign_detector()
     logger.info("Model loading was successful")
     yield
@@ -26,6 +30,7 @@ app = FastAPI(
     redoc_url="/docs/redoc",
 )
 logger.add("logs.log")
+app.mount("/static", StaticFiles(directory=STATIC_PATH), name="static")
 
-
-app.include_router(router)
+app.include_router(api_router)
+app.include_router(frontend_router)
--- src/constants.py
+++ src/constants.py
@@ -1,3 +1,8 @@
 """Common constants"""
+import os
+
 
 SIGN_ID_LIST = [1, 2, 3, 4, 5]
+FRONTEND_PATH = os.path.join(os.path.dirname(__file__), "frontend")
+TEMPLATES_PATH = os.path.join(FRONTEND_PATH, "templates")
+STATIC_PATH = os.path.join(FRONTEND_PATH, "static")
