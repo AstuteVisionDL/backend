35dee50c451ad967e6cbfeb13ff0ccad7e8f0821
Daniil-Solo <daniil.solo1723@gmail.com>
2023-11-27 09:24:40 +0300

    include signs into template

--- src/frontend/router.py
+++ src/frontend/router.py
@@ -2,7 +2,7 @@
 from fastapi.templating import Jinja2Templates
 from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
-from src.constants import TEMPLATES_PATH
+from src.constants import TEMPLATES_PATH, RECOGNIZABLE_SIGNS
 
 router = APIRouter(prefix="")
 templates = Jinja2Templates(directory=TEMPLATES_PATH)
@@ -14,5 +14,5 @@ async def get_home(request: Request):
     Gets homepage
     """
     return templates.TemplateResponse(
-        "index.html", {"request": request}
+        "index.html", {"request": request, "signs": RECOGNIZABLE_SIGNS}
     )
