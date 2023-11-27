5a4930dbf8e2dbadc7cb0fe00a82831ac6574ec3
Daniil-Solo <daniil.solo1723@gmail.com>
2023-11-27 09:23:36 +0300

    add YOLO model

---
+++ src/models/yolo.py
@@ -0,0 +1,29 @@
+"""Yolo model for detection"""
+import io
+from typing import List
+from PIL import Image
+from ultralytics import YOLO
+from src.models.base import BaseSignDetector
+from src.constants import YOLO_PATH, YOLO_THRESHOLD
+
+
+class YOLOSignDetector(BaseSignDetector):
+    """Class for sign detection using YOLO"""
+
+    def __init__(self):
+        self.model = YOLO(YOLO_PATH)
+
+    def recognize_signs(self, image: bytes) -> List[str]:
+        """
+        Generates id list with YOLO model
+        """
+        sign_id_list = []
+        yolo_image = Image.open(io.BytesIO(image))
+        found_signs = self.model.predict(yolo_image)[0]
+        for box in found_signs.boxes:
+            prob = round(box.conf[0].item(), 2)
+            if prob > YOLO_THRESHOLD:
+                class_id = box.cls[0].item()
+                sign_id = found_signs.names[class_id]
+                sign_id_list.append(sign_id)
+        return list(set(sign_id_list))
